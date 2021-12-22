1da84315dfb787d7d56d92c9078116d5c7754ee4
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2021-12-22 04:04:34 +0200

    PW-6 functionality added
    create poll button not yet functionalized

--- client/src/components/CreatePoll.js
+++ client/src/components/CreatePoll.js
@@ -1,21 +1,39 @@
 import React, {useState} from 'react'
-import {CloseButton, Form,Row,Col ,Container, Button, Nav, FormGroup } from "react-bootstrap"
-
+import {CloseButton, Form,Row,Col ,Container, Button, FormGroup } from "react-bootstrap"
+let optionCount =1
 const CreatePoll = () => {
-    const [options, setOptions] = useState([])
+    const[options, setOptions] = useState([])
     const [title, setTitle] = useState("")
-
+    const [Private,setPrivate]=useState(false)
     const handleTitleChange = (event) => {
         setTitle(event.target.value)
     }
     const handleOptionChange=(event)=>{
-
+        console.log(options);
+        let optionId=event.target.id
+        let index
+        for (let i = 0; i < options.length; i++) {
+            if(options[i]==optionId){
+                index=i
+                break
+            }            
+        }
+        options.splice(index,1,event.target.value)
+        setOptions([...options])
     }
     const handleAddOptionClick=() => {
-        setOptions([...options, "New Option"])  
+        setOptions([...options, "New Option"+optionCount])  
+         optionCount ++
     }
-  
-    
+    const handleDeleteOptionClick=(event)=>{
+       let option=event.target.getAttribute("name")
+       document.getElementById(option).value="" 
+       setOptions(options.filter(item =>item!=option) )
+    }
+    const handlePrivateCheckBoxChange=(event)=>{
+        setPrivate(event.target.checked)
+        console.log(event.target.checked);
+    }
     return (
         <Container>
              <Form >
@@ -24,7 +42,6 @@ const CreatePoll = () => {
                     <Form.Group className="mb-3" controlId="pollTitle">
                     <Form.Label>Title</Form.Label>
                     <Form.Control onChange={handleTitleChange} type="text" placeholder="My Poll" /> 
-                   
                     </Form.Group>
                 </Col>
             </Row>
@@ -35,41 +52,37 @@ const CreatePoll = () => {
                         type='checkbox'
                         id={`privateCheck`}
                         label={`Private`}
+                        onChange={handlePrivateCheckBoxChange}
                         />
                     </FormGroup>
                  </Col>   
             </Row>
             <Row className="justify-content-md-center" >
                  <Col sm={4}>
-                    <Form.Group className="mb-3" controlId="pollOptions" inline>
+                    <Form.Group className="mb-3" controlId="pollOptions" >
                     
                     {
                         options.map((option)=>
                         <Row >
                             <Col>
-                            <Form.Control type="text" id={option} onChange={handleOptionChange} placeholder={option} />
+                            <Form.Control type="text" onChange={ handleOptionChange} id={option}  placeholder={option} />
                             </Col>
                             <Col sm={1}>
-                            <CloseButton aria-label="Hide" />
+                            <CloseButton name ={option} onClick={handleDeleteOptionClick} aria-label="Hide" id={'close '+option} />
                             </Col>
                         </Row>
                             
                         ) 
                     }
-                     
                     <Button variant="success"  onClick={handleAddOptionClick}>
                         Add Option     
                     </Button>
                     </Form.Group>
                     </Col>  
             </Row>
-                    
                     <Button variant="primary" type="submit">
                         Create     
-                    </Button>
-      
-                
-            
+                    </Button>            
             <Row>
             </Row>
             </Form>
