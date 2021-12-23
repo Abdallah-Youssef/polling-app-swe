c3ef0616c30f04276c6f5b5b9ed265dee1fadb5f
Abdallah <abdallah.yossf@gmail.com>
2021-12-23 20:25:16 +0200

    Create poll now connected to back end

--- client/src/api/poll.js
+++ client/src/api/poll.js
@@ -1,13 +1,37 @@
 import { apiURL } from "./url";
 
 export const getAllPolls = () => {
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
+    var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
 
-  return fetch(apiURL, requestOptions)
-    .then((response) => response.json())
-    .then(res => res.polls)
-    .catch(() => alert("Failed to reach the server, Please try again later"));
+    return fetch(apiURL, requestOptions)
+        .then((response) => response.json())
+        .then(res => res.polls)
+        .catch(() => alert("Failed to reach the server, Please try again later"));
 };
+
+export const createPoll = (title, Private, options) => {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "question": title,
+        "public": !Private,
+        "choices": options
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    return fetch(apiURL + "/poll/createPoll", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
--- client/src/components/CreatePoll.js
+++ client/src/components/CreatePoll.js
@@ -1,93 +1,106 @@
-import React, {useState} from 'react'
-import {CloseButton, Form,Row,Col ,Container, Button, FormGroup } from "react-bootstrap"
-let optionCount =1
+import React, { useState } from 'react'
+import { CloseButton, Form, Row, Col, Container, Button, FormGroup } from "react-bootstrap"
+import { useNavigate } from 'react-router-dom'
+import { createPoll } from '../api/poll'
+
+let optionCount = 1
 const CreatePoll = () => {
-    const[options, setOptions] = useState([])
+    const navigate = useNavigate()
+    const [options, setOptions] = useState([])
     const [title, setTitle] = useState("")
-    const [Private,setPrivate]=useState(false)
+    const [Private, setPrivate] = useState(false)
     const handleTitleChange = (event) => {
         setTitle(event.target.value)
     }
-    const handleOptionChange=(event)=>{
+    const handleOptionChange = (event) => {
         console.log(options);
-        let optionId=event.target.id
+        let optionId = event.target.id
         let index
         for (let i = 0; i < options.length; i++) {
-            if(options[i]==optionId){
-                index=i
+            if (options[i] === optionId) {
+                index = i
                 break
-            }            
+            }
         }
-        options.splice(index,1,event.target.value)
+        options.splice(index, 1, event.target.value)
         setOptions([...options])
     }
-    const handleAddOptionClick=() => {
-        setOptions([...options, "New Option"+optionCount])  
-         optionCount ++
+    const handleAddOptionClick = () => {
+        setOptions([...options, "New Option" + optionCount])
+        optionCount++
     }
-    const handleDeleteOptionClick=(event)=>{
-       let option=event.target.getAttribute("name")
-       document.getElementById(option).value="" 
-       setOptions(options.filter(item =>item!=option) )
+    const handleDeleteOptionClick = (event) => {
+        let option = event.target.getAttribute("name")
+        document.getElementById(option).value = ""
+        setOptions(options.filter(item => item !== option))
     }
-    const handlePrivateCheckBoxChange=(event)=>{
+    const handlePrivateCheckBoxChange = (event) => {
         setPrivate(event.target.checked)
-        console.log(event.target.checked);
+    }
+
+    const handleCreateClicked = async (event) => {
+        event.preventDefault()
+        optionCount = 1
+        await createPoll(title, Private, options)
+        navigate('/')
     }
     return (
-        <Container>
-             <Form >
-            <Row  className="justify-content-md-center">
-                <Col sm={3}>
-                    <Form.Group className="mb-3" controlId="pollTitle">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control onChange={handleTitleChange} type="text" placeholder="My Poll" /> 
+        <>
+            <h1 className="display-5 w-100 text-center my-4">Create Poll</h1>
+
+            <Container className="py-5 mt-5 w-50 border border-dark rounded">
+                <Form className="w-75 mx-auto">
+
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control onChange={handleTitleChange} type="text" placeholder="My Poll" />
                     </Form.Group>
-                </Col>
-            </Row>
-            <Row  className="justify-content-md-center">
-                <Col sm={1}>
+
+
                     <FormGroup>
-                    <Form.Check 
-                        type='checkbox'
-                        id={`privateCheck`}
-                        label={`Private`}
-                        onChange={handlePrivateCheckBoxChange}
+                        <Form.Check
+                            type='checkbox'
+                            id={`privateCheck`}
+                            label={`Private`}
+                            onChange={handlePrivateCheckBoxChange}
+                            className='my-3 mx-auto'
                         />
                     </FormGroup>
-                 </Col>   
-            </Row>
-            <Row className="justify-content-md-center" >
-                 <Col sm={4}>
-                    <Form.Group className="mb-3" controlId="pollOptions" >
-                    
-                    {
-                        options.map((option)=>
-                        <Row >
-                            <Col>
-                            <Form.Control type="text" onChange={ handleOptionChange} id={option}  placeholder={option} />
-                            </Col>
-                            <Col sm={1}>
-                            <CloseButton name ={option} onClick={handleDeleteOptionClick} aria-label="Hide" id={'close '+option} />
-                            </Col>
-                        </Row>
-                            
-                        ) 
-                    }
-                    <Button variant="success"  onClick={handleAddOptionClick}>
-                        Add Option     
-                    </Button>
+
+                    <hr></hr>
+
+                    <Form.Group className="mb-3"  >
+                        <Form.Label className='mx-3'><h4>Options</h4></Form.Label>
+
+                        {
+                            options.map((option, i) =>
+                                <Row className='my-3' key={i}>
+                                    <Col>
+                                        <Form.Control type="text" onChange={handleOptionChange} id={option} placeholder={option} />
+                                    </Col>
+                                    <Col sm={1}>
+                                        <CloseButton name={option} onClick={handleDeleteOptionClick} aria-label="Hide" id={'close ' + option} />
+                                    </Col>
+                                </Row>
+
+                            )
+                        }
+                        <Button variant="success" onClick={handleAddOptionClick} className='w-25 mx-auto '>
+                            Add Option
+                        </Button>
+
                     </Form.Group>
-                    </Col>  
-            </Row>
-                    <Button variant="primary" type="submit">
-                        Create     
-                    </Button>            
-            <Row>
-            </Row>
-            </Form>
-    </Container>
-       
+
+                    <Button variant="primary" type="submit" onClick={handleCreateClicked}>
+                        Create
+                    </Button>
+                    <Row>
+                    </Row>
+                </Form>
+            </Container>
+
+        </>
     )
 }
 
